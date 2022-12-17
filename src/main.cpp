cd42279d5f2f3102a9afb075ba2c69d4dfbf8615
Erik Hyrkas <erik.hyrkas@gmail.com>
2022-12-17 10:36:07 -0600

    Rough plans for what the main application syntax might look like.

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,20 @@
 using namespace microml;
 
 int main() {
+    // TODO: read from input stream (this can be used to handle API requests or files or whatever later.)
+    // I'll need to make a lexer/parser and think through the details more.
+    // I want to make this similar to sql, in that the underlying details of how
+    // are abstracted away, but I'll try to find ways to give hints to allow people control
+
+    // syntax might be something like:
+    // create <data set type> dataset <name> from <location> [with <format>]
+    // create [<adjective>*] <model type> model <model name> [<model version>] using <data set name>
+    // tune <model name> [<model version>] [as [<model name>] [<model version>]] using <data set name>
+    // retrain <model name> [<model version>] [as [<model name>] [<model version>]] using <data set name>
+
+    // predict using <model name> [<model version] given <input>
+    //               or
+    // infer using <model name> [<model version] given <input>
 
     return 0;
 }
